f5665e57599803852500e64bb4c9314bf2c551c7
Ahmed Wahba <ahmedheshamwahba@hotmail.com>
2024-04-14 21:34:04 +0200

    Initialize Jobs Service

---
+++ job-service/src/test/java/com/guru/jobservice/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.jobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
