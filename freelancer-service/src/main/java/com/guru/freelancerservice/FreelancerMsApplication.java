294e4dea85943bde268bc89449e0ed8f3613b98e
mina magdy <92601097+mina0magdy@users.noreply.github.com>
2024-04-25 21:07:19 +0200

    started freelancer models

---
+++ freelancer-service/src/main/java/com/guru/freelancerservice/FreelancerMsApplication.java
@@ -0,0 +1,13 @@
+package com.guru.freelancerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerMsApplication.class, args);
+	}
+
+}
---
+++ freelancer-service/src/main/java/com/guru/freelancerservice/models/Freelancer.java
@@ -0,0 +1,70 @@
+package com.guru.freelancerservice.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+public class Freelancer {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.UUID
+    )
+    private UUID freelancer_id;
+    private String freelancer_name;
+    private String image_url;
+    private boolean visibility;
+    private int profile_views;
+    private int job_invitations_num;
+    private int available_bids;
+    private double all_time_earnings;
+    private int employers_num;
+    private double highest_paid;
+    private String membership_date;
+    private String tagline;
+    private String bio;
+    private String work_terms;
+    private List<String> attachments=new ArrayList<>();
+    private String user_type;
+    private String website_link;
+    private String facebook_link;
+    private String linkedin_link;
+    private String professional_video_link;
+    private String company_history;
+    private String operating_since;
+
+    public Freelancer() {
+    }
+
+    public Freelancer(UUID freelancer_id, String freelancer_name, String image_url, boolean visibility, int profile_views, int job_invitations_num, int available_bids, double all_time_earnings, int employers_num, double highest_paid, String membership_date, String tagline, String bio, String work_terms, String[] attachments, String user_type, String website_link, String facebook_link, String linkedin_link, String professional_video_link, String company_history, String operating_since) {
+        this.freelancer_id = freelancer_id;
+        this.freelancer_name = freelancer_name;
+        this.image_url = image_url;
+        this.visibility = visibility;
+        this.profile_views = profile_views;
+        this.job_invitations_num = job_invitations_num;
+        this.available_bids = available_bids;
+        this.all_time_earnings = all_time_earnings;
+        this.employers_num = employers_num;
+        this.highest_paid = highest_paid;
+        this.membership_date = membership_date;
+        this.tagline = tagline;
+        this.bio = bio;
+        this.work_terms = work_terms;
+        this.attachments = attachments;
+        this.user_type = user_type;
+        this.website_link = website_link;
+        this.facebook_link = facebook_link;
+        this.linkedin_link = linkedin_link;
+        this.professional_video_link = professional_video_link;
+        this.company_history = company_history;
+        this.operating_since = operating_since;
+    }
+
+
+
+
+}
