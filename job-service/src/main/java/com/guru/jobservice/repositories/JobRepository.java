ed898b2bc196f01efb24651bcd5c8240f38abee0
Ahmed Wahba <ahmedheshamwahba@hotmail.com>
2024-05-03 20:53:18 +0300

    Add create job endpoint

--- job-service/src/main/java/com/guru/jobservice/repositories/JobRepository.java
+++ job-service/src/main/java/com/guru/jobservice/repositories/JobRepository.java
@@ -1,21 +1,36 @@
 package com.guru.jobservice.repositories;
 
 //import com.guru.jobservice.dtos.JobResponse;
+
+import com.guru.jobservice.enums.FixedPriceRange;
+import com.guru.jobservice.enums.PaymentType;
 import com.guru.jobservice.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, UUID> {
-//    @Procedure(procedureName = "get_job_by_id")
-//    Job getJobById(UUID _job_id);
 
     @Query(value = "select * from get_job_by_id(?1)", nativeQuery = true)
     Job getJobById(UUID jobId);
+
+    @Procedure("create_job")
+    void createJob(String _title,
+                   String _description, UUID _category_id,
+                   UUID _subcategory_id, boolean _featured, UUID _client_id,
+                   String _payment_type, String _fixed_price_range,
+                   String _duration, String _hours_per_week,
+                   BigDecimal _min_hourly_rate, BigDecimal _max_hourly_rate,
+                   Date _get_quotes_until, String _visibility,
+                   String[] _skills ,String[] _timezones, String[] _locations
+    );
 
     @Procedure("delete_job_by_id")
     void deleteJobById(UUID jobId);
--- job-service/src/main/java/com/guru/jobservice/services/JobService.java
+++ job-service/src/main/java/com/guru/jobservice/services/JobService.java
@@ -3,7 +3,10 @@ package com.guru.jobservice.services;
 //import com.guru.jobservice.dtos.JobDTO;
 //import com.guru.jobservice.repositories.JobRepository;
 //import com.guru.jobservice.dtos.JobResponse;
+import com.guru.jobservice.dtos.JobRequest;
+import com.guru.jobservice.enums.PaymentType;
 import com.guru.jobservice.exceptions.ResourceNotFoundException;
+import com.guru.jobservice.exceptions.ValidationException;
 import com.guru.jobservice.model.Job;
 import com.guru.jobservice.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -25,15 +26,70 @@ public class JobService {
         this.jobRepository = jobRepository;
     }
 
-//    public void createJob(JobDTO jobDTO) {
-//        //jobRepository.createJob(jobDTO);
-//    }
-//
+
+    public void createJob(JobRequest jobRequest) {
+
+        if (jobRequest.getPaymentType().equals(PaymentType.FIXED.getValue()))
+        {
+            if (jobRequest.getFixedPriceRange() == null)
+            {
+                throw new ValidationException("A price range must be set if payment type is fixed");
+            }
+
+            if(jobRequest.getDuration()!=null || jobRequest.getHoursPerWeek()!=null || jobRequest.getMinHourlyRate()!=null || jobRequest.getMaxHourlyRate()!=null);
+            {
+                throw new ValidationException("Job Duration, Hours per week, min and max hourly rates should be null");
+            }
+        }
+
+        if (jobRequest.getPaymentType().equals(PaymentType.HOURLY.getValue()))
+        {
+            if (jobRequest.getFixedPriceRange() != null)
+            {
+                throw new ValidationException("A price range must be null if payment type is hourly");
+            }
+
+            if(jobRequest.getDuration()==null || jobRequest.getHoursPerWeek()==null || jobRequest.getMinHourlyRate()==null || jobRequest.getMaxHourlyRate()==null);
+            {
+                throw new ValidationException("Job Duration, Hours per week, min and max hourly rates should be SET if payment type is hourly");
+            }
+        }
+
+        if (jobRequest.getMinHourlyRate()!= null && jobRequest.getMaxHourlyRate()!= null )
+        {
+            if (jobRequest.getMinHourlyRate().compareTo(jobRequest.getMaxHourlyRate()) > 0)
+                throw new ValidationException("Minimum Hourly Rate cannot be greater than max Hourly Rate");
+        }
+        jobRepository.createJob(
+                jobRequest.getTitle(),
+                jobRequest.getDescription(),
+                jobRequest.getCategoryId(),
+                jobRequest.getSubcategoryId(),
+                jobRequest.getIsFeatured(),
+                jobRequest.getClientId(),
+                jobRequest.getPaymentType(),
+                jobRequest.getFixedPriceRange(),
+                jobRequest.getDuration(),
+                jobRequest.getHoursPerWeek(),
+                jobRequest.getMinHourlyRate(),
+                jobRequest.getMaxHourlyRate(),
+                jobRequest.getGetQuotesUntil(),
+                jobRequest.getVisibility(),
+                jobRequest.getSkills(),
+                jobRequest.getTimezones(),
+                jobRequest.getLocations()
+        );
+}
+
 //    public void updateJob(UUID jobId, JobDTO jobDTO) {
 //        //jobRepository.updateJob(jobId, jobDTO);
 //    }
-//
+
     public void deleteJobById(UUID jobId) {
+        Job job = jobRepository.getJobById(jobId);
+        if (job == null) {
+            throw new ResourceNotFoundException();
+        }
         jobRepository.deleteJobById(jobId);
     }
 
