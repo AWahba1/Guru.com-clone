b5a7f0d00ed87557c04a81eb3c42baec59ab8014
CptBazinga <116496981+CptBazinga@users.noreply.github.com>
2024-05-09 17:31:10 +0300

    Update PaymentRepository.java

--- payment/src/main/java/com/guru/payment/repositories/PaymentRepository.java
+++ payment/src/main/java/com/guru/payment/repositories/PaymentRepository.java
@@ -2,8 +2,37 @@ package com.guru.payment.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.guru.payment.models.Payment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    @Procedure("CreatePayment")
+    void createPayment(
+            @Param("user_id") String userId,
+            @Param("amount") Double amount,
+            @Param("status") String status);
+
+    @Procedure("UpdatePayment")
+    void updatePayment(
+            @Param("payment_id") Long paymentId,
+            @Param("user_id") String userId,
+            @Param("amount") Double amount,
+            @Param("status") String status,
+            @Param("created_at") Date createdAt);
+
+    @Procedure("DeletePaymentById")
+    void deletePaymentById(@Param("payment_id") Long paymentId);
+
+    @Query(value = "SELECT * FROM GetAllPayments(?1, ?2, ?3, ?4)", nativeQuery = true)
+    List<Payment> getAllPayments(
+            @Param("user_id") String userId,
+            @Param("status") String status,
+            @Param("min_amount") Double minAmount,
+            @Param("max_amount") Double maxAmount);
 }
 
