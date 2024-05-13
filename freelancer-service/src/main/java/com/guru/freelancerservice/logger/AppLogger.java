5be1a4244f7811b9bc1b022edef545b7546e37ca
davidsamy1 <99022820+davidsamy1@users.noreply.github.com>
2024-05-14 00:04:48 +0300

    logger updated

--- freelancer-service/src/main/java/com/guru/freelancerservice/logger/AppLogger.java
+++ freelancer-service/src/main/java/com/guru/freelancerservice/logger/AppLogger.java
@@ -10,17 +10,25 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Aspect
 @Component
 public class AppLogger {
 
     private static final Logger log = LoggerFactory.getLogger(AppLogger.class);
+    private static final String LOG_FILE_PATH = "logs.txt";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     @Pointcut("execution(* com.guru.freelancerservice.services.*.*(..))")
     public void serviceMethods() {
     }
 
-    @Pointcut("execution(* com.guru.freelancerservice.controllers.*.*(..))")
+    @Pointcut("execution(* com.guru.freelancerservice.controller.*.*(..))")
     public void controllerMethods() {
     }
 
@@ -36,18 +44,34 @@ public class AppLogger {
         Object[] args = joinPoint.getArgs();
         ObjectMapper mapper = new ObjectMapper();
         String argsString = mapper.writeValueAsString(args);
+
         log.info("Method {}.{} called with arguments: {}", className, methodName, argsString);
+        logToFile(className, methodName, "Called with arguments: " + argsString);
+
         Object result;
-        try{
+        try {
             result = joinPoint.proceed();
             long elapsedTime = System.currentTimeMillis() - startTime;
             log.info("Method {}.{} returned (Response): {} in {} ms", className, methodName, mapper.writeValueAsString(result), elapsedTime);
-        }
-        catch (Exception e){
+            logToFile(className, methodName, "Returned (Response): " + mapper.writeValueAsString(result) + " in " + elapsedTime + " ms");
+        } catch (Exception e) {
             log.error("Method {}.{} threw an exception: {} ", className, methodName, e.getMessage());
+            logToFile(className, methodName, "Threw an exception: " + e.getMessage());
             throw e;
         }
         return result;
     }
 
+    private void logToFile(String className, String methodName, String logMessage) {
+        try (FileWriter writer = new FileWriter(LOG_FILE_PATH, true)) {
+            writer.write(String.format("[%s] %s.%s - %s\n", getCurrentTimeStamp(), className, methodName, logMessage));
+            writer.flush();
+        } catch (IOException e) {
+            log.error("Error writing to log file: {}", e.getMessage());
+        }
+    }
+
+    private String getCurrentTimeStamp() {
+        return dateFormat.format(new Date());
+    }
 }
