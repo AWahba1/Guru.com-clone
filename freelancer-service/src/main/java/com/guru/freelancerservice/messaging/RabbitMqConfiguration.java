947befbb3b62eaad7089b239ec15dcc906d6b2a3
davidsamy1 <99022820+davidsamy1@users.noreply.github.com>
2024-05-17 14:49:51 +0300

    mq initialization

---
+++ freelancer-service/src/main/java/com/guru/freelancerservice/messaging/RabbitMqConfiguration.java
@@ -0,0 +1,32 @@
+package com.guru.freelancerservice.messaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMqConfiguration {
+
+    @Bean
+    public Queue validateFreelancerId() {
+        return new Queue("validate_freelancer_id");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+
+}
